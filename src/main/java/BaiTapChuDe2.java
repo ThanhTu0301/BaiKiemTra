852a45bdf86f96ed0080595579ad15779fab8d86
your name <abc@gmail.com>
2021-09-08 15:47:03 +0700

    Init BaiTapChuong2

---
+++ src/main/java/BaiTapChuDe2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class BaiTapChuDe2 {
+    public static void main(String[] args){
+        double radius;
+        double area;
+            //Assign a radius
+        radius = 20;
+            //Compute area
+        area = radius * radius * 3.14159;
+            //Display results
+    System.out.println("The area for the circle of radius" + radius + " is " +area);
+    }
+}
---
+++ src/main/java/KhaiBao_Nhap_Xuat.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class KhaiBao_Nhap_Xuat {
+        public static void main(String[] args) {
+        // tạo đối tượng sc thuộc lớp Scanner.
+        Scanner sc = new Scanner(System.in);
+        // Khai báo biến tên
+        String name;
+        // Nhập tên
+        name = sc.nextLine();
+        // Xuất tên
+        System.out.println("My name is "+ name);
+        
+    }
+}
